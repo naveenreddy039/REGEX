7a2dd37c01bf77f0ffb2474ba2e4944bb094970a
naveen <naveenreddy.k07@gmail.com>
2021-11-30 12:59:05 +0530

    first commit

---
+++ UserReg5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UserReg5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkValue;
+            string passwrd = "Abc09@&";
+            //string passwrd1 = "Abcde";
+            Passowrd pass = new Passowrd();
+            checkValue = pass.validatepassword(passwrd);
+            //checkValue = pass.validatepasswordatloneup(passwrd1);
+            if (checkValue)
+            {
+                Console.WriteLine("Pattern valid");
+            }
+            else
+            {
+                Console.WriteLine("Pattern not valid");
+            }
+            
+        }
+    }
+}
